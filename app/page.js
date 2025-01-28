86c8cc3b864894c4b94285089746916fb199a8f4
tejs9k <tejaswipemmasani8@gmail.com>
2025-01-28 11:33:22 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Box, Button, Stack, Typography, TextField, Modal } from '@mui/material';
 import { firestore } from '@/firebase';
